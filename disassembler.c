5aee0d543abb78098cde95e461409bcc669ac0ca
Jack Pearson <99jackpearson@gmail.com>
2020-02-04 14:53:42 -0800

    made disassembler executable

--- disassembler.c
+++ disassembler.c
@@ -3,7 +3,29 @@
 #include <stdio.h>
 #include <assert.h>
 #include <inttypes.h>
+
 #include "disassembler.h"
+
+int main(int argc, char **argv) {
+    if (argc > 4 || argc == 1) {
+        printf("Usage: ./dsm <inFile> [outFile]\n");
+        exit(-1);
+    }
+    char *inFileName = argv[1];
+    char outFileName[50];
+    if (argc == 2) {
+        strcpy(outFileName, inFileName);
+        char *period = strchr(outFileName, '.');
+        if (period == NULL) {
+            strcpy(outFileName, "dsm.dsm");
+        } else {
+            strcpy(period+1, "dsm");
+        }
+    } else if (argc == 3) {
+        strcpy(outFileName, argv[2]);
+    }
+    disassembleFile(inFileName, outFileName);
+}
 
 void disassembleFile(char const *inFileName, char const *outFileName) {
     FILE* inFile = fopen(inFileName, "r");
